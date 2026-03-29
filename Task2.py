75bbddea4af4d188d1cefdaf1aa174d3ff8f1af2
Saideep-R-26 <s36751@caseygrammar.vic.edu.au>
2026-03-29 00:33:37 +0000

    Finished Task 2

--- Task2.py
+++ Task2.py
@@ -1,7 +1,9 @@
 # Task 2: James Bond
 def main():
     #Write your code for input here
-    
+    firstName = input("What is your first name? ")
+    lastName = input("What is your last name? ")
+    print("My name is " + lastName + ", " + firstName + " " + lastName)
 
 
     # End of your code for input here
