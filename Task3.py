564e5018299ede2058c40bd7c9b08e3ffbe30c75
Saideep-R-26 <s36751@caseygrammar.vic.edu.au>
2026-03-29 00:37:22 +0000

    Finished Task3

--- Task3.py
+++ Task3.py
@@ -3,9 +3,9 @@
 # Write a program that asks the user
 def main():
     #Write your code for input here
-    
-
-
+    dog_years = int(input('Dog Years: '))
+    human_years = dog_years * 7
+    print(f'Human years = {human_years}')
     # End of your code for input here
 
 if __name__ == '__main__':
